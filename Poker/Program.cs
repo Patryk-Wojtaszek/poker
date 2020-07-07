a3c820dc78d6ff3a27369c6af0b052831f83b9f9
sniqq1 <pat.wojtaszek@gmail.com>
2020-07-07 13:10:20 +0200

    ddsdsd

--- Poker/Program.cs
+++ Poker/Program.cs
@@ -8,11 +8,27 @@ namespace Poker
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Zagrajmy w Pokera! ");
-            Karta karta = new Karta();
-            karta.mojRodzaj = Karta.Rodzaj.Karo;
-            karta.mojWartość = Karta.Wartość.As;
-            Console.WriteLine(karta);
+            Console.WriteLine("Zagrajmy w Texas Holdem Poker! ");
+            Console.WriteLine("1- zacznij grać, 2- co to jest Texas Holdem?, 0 - wyjdź z programu ");
+            string a = Console.ReadLine();
+            switch(a)
+            {
+                case "0":
+                    Environment.Exit(0);
+                    break;
+                case "1":
+                    break;
+                case "2":
+                    break;
+                default:
+                    Console.WriteLine("Niepoprawna Komenda, wpisz 0, 1, lub 2 ");
+                    break;
+            }
+            /* Karta karta = new Karta();
+             karta.mojRodzaj = Karta.Rodzaj.Karo;
+             karta.mojWartość = Karta.Wartość.As;
+             Console.WriteLine(karta);
+             */
         }
     }
 }
---
+++ PokerClassLibrary/Rozdaj.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace PokerClassLibrary
+{
+    public class Rozdaj : TaliaKart
+    {
+        private List<Karta> gracz;
+        private List<Karta> komputer;
+        private List<Karta> flop;
+
+        public Rozdaj()
+        {
+            gracz = new List<Karta>(2);
+            komputer = new List<Karta>(2);
+            flop = new List<Karta>(5);
+        }
+    }
+}
